b410e10205548e04a2ccfbfc8ef20b66c3baddbf
Deepak <deepak.me005@gmail.com>
2026-03-26 19:14:39 +0530

    added seo features and updated project structure

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html", "./script.js", "./projects.json"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
+      colors: {
+        'brand-black': '#1a1a1a',
+        'brand-gray': '#f4f4f4',
+        'brand-beige': '#f5f5f0',
+      }
+    }
+  },
+  plugins: [],
+}
